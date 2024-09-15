216b2ac4de051012fd8a5e4d85c7f5f736ea7193
luuminhnhat12 <nguoidep433@gmail.com>
2024-09-15 18:39:35 +0700

    init project

---
+++ flutter_application_flutter_multi_language/README.md
@@ -0,0 +1,3 @@
+# flutter_application_flutter_multi_language
+
+A new Flutter project.
---
+++ flutter_application_flutter_multi_language/android/app/src/main/kotlin/com/example/flutter_application_flutter_multi_language/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_flutter_multi_language
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
