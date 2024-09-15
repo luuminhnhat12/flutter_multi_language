3f902a1d4e948d23f22c4f6e099ce2347cd46d5f
luuminhnhat12 <nguoidep433@gmail.com>
2024-09-15 20:57:39 +0700

    done

--- flutter_application_flutter_multi_language/lib/main.dart
+++ flutter_application_flutter_multi_language/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_flutter_multi_language/l10n/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +11,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return  MaterialApp(
+  title: 'Localizations Sample App',
+  locale: const Locale('en'),
+  localizationsDelegates: [
+    AppLocalizations.delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
+  supportedLocales: L10n.all,
+  home: MyHomePage(),
+  );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Localizations Sample App'),
       ),
+      body: Center(
+        child:Text(AppLocalizations.of(context)!.hello),
+      ),
+      
     );
   }
 }
